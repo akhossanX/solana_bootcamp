69e28a14dcae0403eac31820e69585f28342ef68
Abdelilah <abdelilah.khossan@gmail.com>
2022-11-02 00:22:49 +0100

    Added fizz_buzz function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+fn fizz_buzz() {
+	let mut fizz_buzz_counter:i16 = 0;
+
+	for count in 1..=301 {
+		if count % 3 == 0 {
+			println!("fizz");
+		} else if count % 5 == 0 {
+			println!("buzz");
+		} else {
+			println!("fizz buzz");
+			fizz_buzz_counter += 1;
+		}
+	}
+	println!("{}", fizz_buzz_counter);
+}
